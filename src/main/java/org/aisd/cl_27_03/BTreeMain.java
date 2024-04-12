ea4a770faaa073d185f76aef0516ea1eb06873fb
enya <anna361362@gmail.com>
2024-04-12 22:26:45 +0300

    cl 27.03.24

---
+++ src/main/java/org/aisd/cl_27_03/BTreeMain.java
@@ -0,0 +1,12 @@
+package org.aisd.cl_27_03;
+
+import java.util.Stack;
+import java.util.function.Consumer;
+
+public class BTreeMain {
+    public static Node root;
+    public static void main(String[] args) {
+        int[] arr = new int[] {8, 5, 11, 3, 6, 10, 12, 1};
+    }
+    //public static void
+}
