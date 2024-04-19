101681db2f042e629f3b1e681355c2eefd8337a9
enya <anna361362@gmail.com>
2024-04-19 18:54:59 +0300

    dz 13_03_23 update

--- src/main/java/org/example/dz_13_03/Main.java
+++ src/main/java/org/example/dz_13_03/Main.java
@@ -2,20 +2,38 @@ package org.example.dz_13_03;
 // прочитать файл и записать как массив адресов
 
 import java.io.*;
-import java.util.Collections;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("data-1712124662054.csv")));
+        InputStreamReader in = new InputStreamReader(new FileInputStream("data-1712124662054.csv"), StandardCharsets.UTF_8);
         Map<String, String> map = new HashMap<>();
-        do {
-            String[] strings = reader.readLine().split(",");
-            map.put(strings[0], strings[1]);
+
+        while (true) {
+            StringBuilder key = new StringBuilder();
+            StringBuilder value = new StringBuilder();
+            int iByte = in.read();
+            if (iByte != -1) {
+                do {
+                    key.append((char) iByte);
+                    iByte = in.read();
+                }
+                while (iByte != (int) ',');
+            }
+            else break;
+            iByte = in.read();
+            if (iByte != -1) {
+                do {
+                    iByte = in.read();
+                    value.append((char) iByte);
+                }
+                while (iByte != (int) '\n');
+            }
+            else break;
+            map.put(key.deleteCharAt(key.length()-1).toString(), value.deleteCharAt(value.length() - 1).toString());
         }
-        while (reader.ready());
         map.forEach((k, v) -> System.out.println(k + ": " + v));
     }
 }
